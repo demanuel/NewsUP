demanuel/NewsUP

commit 97cb6d8c5fef81c0edbeb17bec5bddb8fd62fbed

Changed some discrepancies in the options that were specified in the README and what it should have been used

newsup.pl
@@ -152,7 +152,7 @@ sub _parse_command_line{
 	     'headerCheckPort=i'=>\$headerCheckPort,
 	     'headerCheckUserName=s'=>\$headerCheckUserName,
 	     'headerCheckPassword=s'=>\$headerCheckPassword,
-	     'headerCheckRetries|retry=i'=>\$headerCheckRetries,
+	     'headerCheckRetries|retries=i'=>\$headerCheckRetries,
 	     'uploadsize=i'=>\$NNTP_MAX_UPLOAD_SIZE
 	    );
 
@@ -209,7 +209,7 @@ sub _parse_command_line{
 	}
       }
       if (!defined $headerCheckUserName || $headerCheckUserName eq '') {
-	if (exists $config->{headerCheck}{username} && $config->{headerCheck}{username} ne ''){
+	if (exists $config->{headerCheck}{user} && $config->{headerCheck}{user} ne ''){
 	  $headerCheckUserName = $config->{headerCheck}{username};
 	}else {
 	  $headerCheckUserName=$username;

commit b3815142d5c5c6ab2386591b1a843c3bd1b16c40

FIxed the bug introduced by commit 97cb6d8c5fef81c0edbeb17bec5bddb8fd62fbed

newsup.pl
@@ -210,7 +210,7 @@ sub _parse_command_line{
       }
       if (!defined $headerCheckUserName || $headerCheckUserName eq '') {
 	if (exists $config->{headerCheck}{user} && $config->{headerCheck}{user} ne ''){
-	  $headerCheckUserName = $config->{headerCheck}{username};
+	  $headerCheckUserName = $config->{headerCheck}{user};
 	}else {
 	  $headerCheckUserName=$username;
 	}